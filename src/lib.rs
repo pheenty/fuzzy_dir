58b392ea9d5ff131a77c9c29fce6a88570a29050
Ted Lukin <fedorlukin2006@gmail.com>
2026-08-06 02:56:51 +0700

    prettier?

--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,11 @@ pub use split::split;
 
 const CHARVAL_NOM: f32 = 25.;
 const CHARVAL_MAX_DENOM: f32 = 5.;
-const FULL_MATCH_SCORE: f32 = 120.;
+const FULL_MATCH_SCORE: f32 = 100.;
 const CHAR_HIT_MULT: f32 = 2.;
 const CHAR_PART_HIT_MULT: f32 = 1.;
 const CHAR_MISS_PENALTY: f32 = -5.;
-const WORD_MATCH_SCORE: f32 = 20.;
+const WORD_MATCH_SCORE: f32 = 25.;
 
 /// Creates a score of how much the input and the pattern match
 /// The higher the score the better. There is no max score.
