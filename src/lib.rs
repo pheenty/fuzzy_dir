54c259bf1a4e0df4d21f4fd49c865415ba806202
Ted Lukin <fedorlukin2006@gmail.com>
2026-08-06 02:52:51 +0700

    wait

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ pub use split::split;
 
 const CHARVAL_NOM: f32 = 25.;
 const CHARVAL_MAX_DENOM: f32 = 5.;
-const FULL_MATCH_SCORE: f32 = 20.;
+const FULL_MATCH_SCORE: f32 = 75.;
 const CHAR_HIT_MULT: f32 = 2.;
 const CHAR_PART_HIT_MULT: f32 = 1.;
 const CHAR_MISS_PENALTY: f32 = -2.;
