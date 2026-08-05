38ba886ad4524d6d06606a27e522c74cbe695e9b
Ted Lukin <fedorlukin2006@gmail.com>
2026-08-06 02:55:49 +0700

    wait3

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ const CHARVAL_MAX_DENOM: f32 = 5.;
 const FULL_MATCH_SCORE: f32 = 120.;
 const CHAR_HIT_MULT: f32 = 2.;
 const CHAR_PART_HIT_MULT: f32 = 1.;
-const CHAR_MISS_PENALTY: f32 = -2.;
+const CHAR_MISS_PENALTY: f32 = -5.;
 const WORD_MATCH_SCORE: f32 = 20.;
 
 /// Creates a score of how much the input and the pattern match
